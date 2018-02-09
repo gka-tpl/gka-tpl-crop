bbc9e29e4d743362cd3c50aaeb046458023fee8a
joeyguo <lgsincy@gmail.com>
2018-02-09 13:56:17 +0800

    1.2.0

--- index.js
+++ index.js
@@ -1,13 +1,25 @@
+var gkaUtils = require('gka-utils'),
+    writeSync = gkaUtils.file.writeSync;
 var html = require("./lib/html"),
 	css = require("./lib/css");
 
-module.exports = function (data, opts, tool) {
-    var prefix = opts.prefix,
-        frameduration = opts.frameduration;
+module.exports = function (data, opts, cb) {
+    var dir = opts.imageDir;
 
-    var names = tool.getNames();
-    
-    tool.writeFile("gka.css", css(data, prefix, frameduration));
-    tool.writeFile("gka.html", html(names, prefix));
+    function run(data, opts, key) {
+        var name = (key? key + '-' : '') + 'gka',
+            cssName = name + '.css',
+            htmlName = name + '.html';
+
+        writeSync([dir, '..', cssName], css(data, opts));
+        writeSync([dir, '..', htmlName], html(data, opts, cssName));
+    }
+
+    run(data, opts);
+
+    // 对每个子目录都进行处理
+    gkaUtils._.effectSubFolderSync(run, data, opts);
+
+    cb && cb();
 };
 
